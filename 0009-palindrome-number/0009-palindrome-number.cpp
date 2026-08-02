19e10bc1f9c82052d6ec38acd63f95dfe04dca5f
Tajinder singh <tajinderdursal@gmail.com>
2026-08-02 10:13:54 +0530

    Time: 4 ms (30.95%), Space: 8.7 MB (20.61%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp=x;
+        double reverse=0;
+    while(temp>0){
+        int a=temp%10;
+      reverse=reverse*10+a;
+     temp=temp/10;
+    }
+    if (reverse==x){
+        return true;
+    }
+    return false;
+    }
+};
