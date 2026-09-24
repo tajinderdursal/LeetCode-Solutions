9965582bf1be229a7fb4ecbfba017976d487d285
Tajinder singh <tajinderdursal@gmail.com>
2026-09-24 14:47:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (6.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int threshold=(nums.size())/2;
-        unordered_map<int, int > frequencies;
+        unordered_map<int,int > frequencies;
         for(int i :nums){
             frequencies[i]++;
             if(frequencies[i]>threshold){
                 return i;
             }
-
         }
         return -1;
         
