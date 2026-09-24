36012340400e3c0279b575a8adae880084c6e9a9
Tajinder singh <tajinderdursal@gmail.com>
2026-09-24 14:31:12 +0530

    Time: 4 ms (20.16%), Space: 42.1 MB (10%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int threshold=(nums.size())/2;
+        map<int, int > frequencies;
+        for(int i :nums){
+            frequencies[i]++;
+            if(frequencies[i]>threshold){
+                return i;
+            }
+
+        }
+        return -1;
+        
+    }
+};
